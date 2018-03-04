69e2a0df562310acb5724451a86b22c1908bfe8b
Cyril COLINET <cyril.colinet@epitech.eu>
2018-03-04 12:18:45 +0100

    fix text position

--- src/assets/setup/options_sliders.c
+++ src/assets/setup/options_sliders.c
@@ -7,26 +7,33 @@
 
 # include "cook.h"
 
+static slider_t extra_config(cook_t *cook, slider_t conf)
+{
+	slider_t c = conf;
+	sfVector2f ori = { conf.range.y + 43, conf.mid_axis - 25 };
+
+	sfSprite_setPosition(c.btn->sprite, c.btn->pos);
+	sfText_setPosition(c.text, ori);
+	sfText_setFont(c.text, cook->font);
+	sfText_setCharacterSize(c.text, 40);
+
+	return (c);
+}
+
 void setup_volume_options_slider(cook_t *cook)
 {
 	slider_t conf;
 	sfVector2f pos = { 100, 100 };
-	sfVector2f ori = { 100, 100 };
 
 	conf.type = slideVolume;
-	conf.mid_axis = pos.y = ori.y = 250;
+	conf.mid_axis = pos.y = 250;
 	conf.range.x = pos.x = 760;
-	conf.range.y = ori.x = 1040;
-	ori.x += 45;
-	ori.y -= 25;
+	conf.range.y = 1040;
 	conf.state = gameOnSettings;
 	conf.onSlide = slider_volume_callback;
 	conf.btn = get_button(cook, btnVolume, gameOnSettings);
 	conf.text = sfText_create();
 	conf.btn->pos = pos; // TODO: fix decallage with origin and btn position
-	sfSprite_setPosition(conf.btn->sprite, conf.btn->pos);
-	sfText_setPosition(conf.text, ori);
-	sfText_setFont(conf.text, cook->font);
-	sfText_setCharacterSize(conf.text, 40);
-	add_slider(cook, conf);
+
+	add_slider(cook, extra_config(cook, conf));
 }
